6bfdb4c63d63b2f4963b2c6b2c666b770f605d22
charles378 <charley.jaru250@gmail.com>
2024-10-04 23:04:56 -0300

    criei at tela inicial pega mentade

--- util/rotas.py
+++ util/rotas.py
@@ -5,15 +5,23 @@ from tela import maini
 
 
 def main(page: ft.Page):
-    page.appbar =  ft.AppBar(
-                title=ft.Text('Lista de casamento'),
-                center_title=True,
-                bgcolor=ft.colors.BLUE
-            )
+    page.appbar = ft.AppBar(
+                    title=ft.Text('Lista de casamento'),
+                    center_title=True,
+                    bgcolor=ft.colors.BLUE
+                ),
+    
     def route_change(route):
         page.views.clear()
         page.views.append(
-            ft.View(route="/", controls=[maini(page)],)
+            ft.View(
+                route="/",
+                appbar=ft.AppBar(
+                    title=ft.Text('Lista de casamento'),
+                    
+                    bgcolor=ft.colors.SURFACE_VARIANT
+                ), 
+                controls=[maini(page)],)
         )
 
         if page.route == "/store":
--- util/tela.py
+++ util/tela.py
@@ -2,25 +2,36 @@ import flet as ft
 
 
 def  maini(page: ft.Page):
+    def validasao_senha(e):
+        pass
+    
+    avatar = ft.CircleAvatar(
+        col=3,  # cantida de coluna que vai o cupar
+        foreground_image_url='https://www.naoviu.com.br/wp-content/uploads/2023/08/Spacex.jpg',
+        height=240,
+        width=180
+    )
     
     return ft.Container(
         alignment=ft.alignment.center,
-        content=ft.Column(controls=[
-            #page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-            ft.AppBar(
-                title=ft.Text('Lista de casamento'),
-                center_title=True,
-                bgcolor=ft.colors.BLUE
-            ),
 
+        content=ft.Column(
+            controls=[
             #foto do casal
-
+            avatar,
             #butao minha lista so quem ten asseso e o casal eo butao do convidado aulado
             ft.Container(
-                content=ft.Row([ft.TextButton('Minha lista',on_click=lambda _: page.go('/store')), ft.TextButton('Convidado')],alignment=ft.alignment.center),
+                
+                content=ft.Row([ft.TextButton('Minha lista',on_click=lambda _: page.go('/store')), 
+                                ft.TextButton('Convidado')],
+                                alignment=ft.alignment.center, 
+                                width=200,),             
                 alignment=ft.alignment.center,
                 width=200,
                 bgcolor=ft.colors.WHITE12,
-                border_radius=50)])
+                border_radius=50,
+                )
+              ]
+            )
     )
 
